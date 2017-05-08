ee175ab07b2b1c5c46e9e6ff6a8440bded86e55c
Aline Vianna <aline@alinevianna.com.br>
2017-05-07 22:04:45 -0300

    testando configuração global do commit

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@
  * Feed Reader API to grab RSS feeds as JSON object we can make
  * use of. It also uses the Handlebars templating library and
  * jQuery.
- */
+ **/
 
 // The names and URLs to all of the feeds we'd like available.
 var allFeeds = [
